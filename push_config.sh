7a4e9c81d9e8b6f9901e5fb86f240bd9633b98a0
Nathan Muggli <nmuggli@google.com>
2026-09-22 10:05:40 -0600

    Create a script to push a printer config to a device (#20)

---
+++ push_config.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# Script to push a printer configuration file to an Android device for a specific user.
+#
+
+set -euo pipefail
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <config_file> <user_id>"
+    exit 1
+fi
+
+filename="$1"
+user="$2"
+
+adb push $filename /data/user/$user/com.google.virtualprinter/files/printer_config.json
+uid=$(adb shell pm list packages -U --user $user com.google.virtualprinter | tr -d '\r' | sed -ne 's/package:com.google.virtualprinter *uid:\([0-9][0-9]\)/\1/p')
+adb shell chown -R $uid:$uid /data/user/$user/com.google.virtualprinter/files
+adb shell restorecon -Rv /data/user/$user/com.google.virtualprinter/files/printer_config.json
